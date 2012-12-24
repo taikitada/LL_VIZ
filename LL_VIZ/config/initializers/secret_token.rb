dcd880bcd8b59e3191e9009f1e604eb9f87376e8
TaikiTada <mr.strangebird@gmail.com>
2012-12-24 14:36:39 +0900

    web可視化用にrailsを導入

---
+++ LL_VIZ/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LLViz::Application.config.secret_token = '320acca92c2b9743c76678276023c7ef87f65c91d989c22dfcf6d4823ff8094c1a8c4c3ab74a93fec0547f453c91d8fcde9d7d00952f2a5f19bfc6452c581939'
